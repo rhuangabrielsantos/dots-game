42d2e932be3bc9a46fadc2df0dd2f1eb2de20b4f
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-02-05 11:44:46 -0300

    :lipstick: Added global style and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: '#F6F1EB',
+    secondary: '#BCC1C7',
+    tertiary: '#202124',
+
+    default: '#BCC1C7',
+    red: '#FF2329',
+    blue: '#0030f3',
+    gray: '#828282',
+    white: '#FFfFf0',
+  },
+}
