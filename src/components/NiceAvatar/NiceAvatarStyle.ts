57bb182e958d84f3e4414804b0a7b5db32e9bcf2
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-03-16 13:18:46 -0300

    fix: Fixed nice avatar

--- src/components/NiceAvatar/NiceAvatarStyle.ts
+++ src/components/NiceAvatar/NiceAvatarStyle.ts
@@ -1,26 +1,11 @@
 import styled from 'styled-components'
 
-import { Colors } from '@/interfaces/Player'
-
-interface NiceAvatarStyleProps {
-  isSecondPlayer?: boolean
-  size?: 'small' | 'large' | undefined
-  isMyTurn?: boolean
-  myColor?: Colors
-}
-
-export const NiceAvatarStyle = styled.div<NiceAvatarStyleProps>`
+export const NiceAvatarStyle = styled.div`
   > div {
-    width: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
-    height: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
+    width: 7rem;
+    height: 7rem;
 
     transition: filter 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-
-    transform: ${(props) => (props.isSecondPlayer ? 'scaleX(-1)' : '')};
-
-    filter: ${(props) => (props.isMyTurn ? '' : 'grayscale(90%)')};
-    box-shadow: ${(props) =>
-      props.isMyTurn ? `0 0 0 10px ${props.myColor}` : ''};
 
     ${(props) => props.theme.media.mobile} {
       width: 5rem;
@@ -28,3 +13,21 @@ export const NiceAvatarStyle = styled.div<NiceAvatarStyleProps>`
     }
   }
 `
+
+// import { Colors } from '@/interfaces/Player'
+// interface NiceAvatarStyleProps {
+//   isSecondPlayer?: boolean
+//   size?: 'small' | 'large' | undefined
+//   isMyTurn?: boolean
+//   myColor?: Colors
+// }
+// width: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
+// height: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
+
+// transition: filter 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+
+// transform: ${(props) => (props.isSecondPlayer ? 'scaleX(-1)' : '')};
+
+// filter: ${(props) => (props.isMyTurn ? '' : 'grayscale(90%)')};
+// box-shadow: ${(props) =>
+//   props.isMyTurn ? `0 0 0 10px ${props.myColor}` : ''};
