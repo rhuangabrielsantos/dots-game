0d05572d511ac9cf09b3b981222262024a31a858
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-03-16 11:12:51 -0300

    add: Created button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+
+import { SfxContext } from '@/contexts/SfxContext'
+
+import { ButtonProps } from './ButtonProps'
+import { ButtonStyle } from './ButtonStyle'
+
+export function Button(props: ButtonProps) {
+  const { tickSfx } = useContext(SfxContext)
+
+  return (
+    <ButtonStyle {...props} onMouseEnter={() => tickSfx()}>
+      {props.children}
+    </ButtonStyle>
+  )
+}
---
+++ src/components/Button/ButtonStyle.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const ButtonStyle = styled.button<{ color?: string }>`
+  font-size: 24px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.color};
+  border: none;
+  border-radius: 10px;
+  padding: 20px 30px;
+  cursor: pointer;
+
+  transition: shadow 0.3s;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover:enabled {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  }
+
+  &:disabled {
+    opacity: 0.4;
+    cursor: not-allowed;
+  }
+`
