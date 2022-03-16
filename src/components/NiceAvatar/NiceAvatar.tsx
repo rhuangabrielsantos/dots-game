73e99ce91efce9a4759af125b315bc1880158e19
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-03-16 12:23:29 -0300

    fix: Fixed nice avatar style

--- src/components/NiceAvatar/NiceAvatar.tsx
+++ src/components/NiceAvatar/NiceAvatar.tsx
@@ -1,14 +1,17 @@
+import Avatar from 'react-nice-avatar'
+
 import { NiceAvatarProps } from './NiceAvatarProps'
 import { NiceAvatarStyle } from './NiceAvatarStyle'
 
 export function NiceAvatar(props: NiceAvatarProps) {
   return (
     <NiceAvatarStyle
-      {...props.avatarConfig}
       size={props.size}
       isSecondPlayer={props.isSecondPlayer}
       isMyTurn={props.isMyTurn ?? false}
       myColor={props.player?.color}
-    />
+    >
+      <Avatar {...props.avatarConfig} />
+    </NiceAvatarStyle>
   )
 }
--- src/components/NiceAvatar/NiceAvatarStyle.ts
+++ src/components/NiceAvatar/NiceAvatarStyle.ts
@@ -1,4 +1,3 @@
-import Avatar from 'react-nice-avatar'
 import styled from 'styled-components'
 
 import { Colors } from '@/interfaces/Player'
@@ -10,20 +9,22 @@ interface NiceAvatarStyleProps {
   myColor: Colors
 }
 
-export const NiceAvatarStyle = styled(Avatar)<NiceAvatarStyleProps>`
-  width: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
-  height: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
+export const NiceAvatarStyle = styled.div<NiceAvatarStyleProps>`
+  > div {
+    width: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
+    height: ${({ size }) => (size === 'small' ? '7rem' : '10rem')};
 
-  transition: filter 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+    transition: filter 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
 
-  transform: ${(props) => (props.isSecondPlayer ? 'scaleX(-1)' : '')};
+    transform: ${(props) => (props.isSecondPlayer ? 'scaleX(-1)' : '')};
 
-  filter: ${(props) => (props.isMyTurn ? '' : 'grayscale(90%)')};
-  box-shadow: ${(props) =>
-    props.isMyTurn ? `0 0 0 10px ${props.myColor}` : ''};
+    filter: ${(props) => (props.isMyTurn ? '' : 'grayscale(90%)')};
+    box-shadow: ${(props) =>
+      props.isMyTurn ? `0 0 0 10px ${props.myColor}` : ''};
 
-  ${(props) => props.theme.media.mobile} {
-    width: 5rem;
-    height: 5rem;
+    ${(props) => props.theme.media.mobile} {
+      width: 5rem;
+      height: 5rem;
+    }
   }
 `
