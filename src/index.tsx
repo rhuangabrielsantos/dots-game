b3926f2261d33ff881eda2460166a18cd5d6871c
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-03-22 21:22:32 -0300

    fix: Fix Service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-import registerServiceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-registerServiceWorker()
+serviceWorkerRegistration.register()
