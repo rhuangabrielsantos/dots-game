06322d2f02d7404540899099c6f1990f070a669e
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2022-04-03 11:21:16 -0300

    refactor: Refactor dark mode tests

--- cypress/integration/darkMode.spec.ts
+++ cypress/integration/darkMode.spec.ts
@@ -1,12 +1,19 @@
 describe('Dark mode tests', () => {
-  it('test dark mode, given user click, should change colors', () => {
+  it('test dark mode, given user click, when dark mode is disabled, should activate dark mode', () => {
     cy.visit('/')
+
+    cy.get('#dark-mode-button').should('have.attr', 'aria-checked', 'false')
 
     cy.get('#dark-mode-button').click()
 
     cy.get('#dark-mode-button').should('have.attr', 'aria-checked', 'true')
     cy.get('#root').should('have.css', 'background-color', 'rgb(32, 33, 36)')
+  })
 
+  it('test dark mode, given user click, when dark mode is enabled, should disable dark mode', () => {
+    cy.visit('/')
+
+    cy.get('#dark-mode-button').click()
     cy.get('#dark-mode-button').click()
 
     cy.get('#dark-mode-button').should('have.attr', 'aria-checked', 'false')
