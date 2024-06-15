36fd38c1e860602daffe648b1e7e135f892226e9
Rhuan Santos <rhuan.santos@db1.com.br>
2024-06-15 12:18:15 -0300

    add sentry logs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "db1-group",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
